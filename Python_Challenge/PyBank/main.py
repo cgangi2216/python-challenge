fb540eebc02d668d23cc857170eded51e82f7932
Christina Gangi <christina@buzzella.com>
2021-01-12 22:46:19 -0500

    Completed PyPoll analysis & final touches on PyBank

--- Python_Challenge/PyBank/main.py
+++ Python_Challenge/PyBank/main.py
@@ -11,69 +11,69 @@ with open(budget_path,'rt') as budget_file:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(budget_file, delimiter=',')
 
-    # Read headers into a variable for reference while coding
+    # Read headers into a variable
     budget_header = next(csvreader)
     #print(f"Header: {budget_header}")
 
     # Create dictionary
     budget_dict = {}
 
-    # Create variables
+    # Create variables for calculating change in Profit/Losses
     row_ct = 1
     profit_loss_previous = 0
 
-    # Read csv into dictionary & calculate change in profit/losses
+    # Read csv into dictionary & calculate change in Profit/Losses
     for row in csvreader:
         # If this is the 1st row, skip it. If not, compare Profit/Losses to last month.
         if row_ct == 1:
-            profit_loss_change = "N/A"
+            profit_loss_change = None
         else:
             profit_loss_change = int(row[1]) - int(profit_loss_previous)
 
         budget_dict[row[0]] = {budget_header[1]: int(row[1]), "Change": profit_loss_change}
         
         # Set previous Profit/Losses variable for calculation in next row
-        profit_loss_previous = int(row[1])
+        profit_loss_previous = row[1]
         
         # Increment row count variable
-        row_ct = row_ct+1
+        row_ct += 1
 
-    #print(budget_dict)
+    #close csv file
+    budget_file.close()
 
-    # Calculate the total number of months included in the dataset
-    total_months = len(budget_dict)
+#print(budget_dict)
 
-    # Calculate the net total amount of "Profit/Losses" over the entire period
-    total_profit_losses = sum([budget_dict[row]["Profit/Losses"] for row in budget_dict])
+# Calculate the total number of months included in the dataset
+total_months = len(budget_dict)
 
-    # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-    avg_change = round(sum([budget_dict[row]["Change"] for row in budget_dict if budget_dict[row]["Change"] != "N/A"])/(total_months-1),2)
+# Calculate the net total amount of "Profit/Losses" over the entire period
+total_profit_losses = sum([budget_dict[row]["Profit/Losses"] for row in budget_dict])
 
-    # Find the greatest increase in profits (date and amount) over the entire period
-    greatests_increase = max([budget_dict[row]["Change"] for row in budget_dict if budget_dict[row]["Change"] != "N/A"])
-    greatests_increase_month = [row for row in budget_dict if budget_dict[row]["Change"] == greatests_increase][0]
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+avg_change = round(sum([budget_dict[row]["Change"] for row in budget_dict if budget_dict[row]["Change"] != None])/(total_months-1),2)
 
-    # Find the greatest decrease in losses (date and amount) over the entire period
-    greatests_decrease = min([budget_dict[row]["Change"] for row in budget_dict if budget_dict[row]["Change"] != "N/A"])
-    greatests_decrease_month = [row for row in budget_dict if budget_dict[row]["Change"] == greatests_decrease][0]
+# Find the greatest increase in profits (date and amount) over the entire period
+greatests_increase = max([budget_dict[row]["Change"] for row in budget_dict if budget_dict[row]["Change"] != None])
+greatests_increase_month = [row for row in budget_dict if budget_dict[row]["Change"] == greatests_increase][0]
+
+# Find the greatest decrease in losses (date and amount) over the entire period
+greatests_decrease = min([budget_dict[row]["Change"] for row in budget_dict if budget_dict[row]["Change"] != None])
+greatests_decrease_month = [row for row in budget_dict if budget_dict[row]["Change"] == greatests_decrease][0]
     
+# Build output text
+output_text = 'Financial Analysis\n'
+output_text = output_text+F'----------------------------------------------------\n'
+output_text = output_text+F'Total Months: {total_months}\n'
+output_text = output_text+F'Total Profit/Losses: ${total_profit_losses}\n'
+output_text = output_text+F'Average Change in Profits: ${avg_change}\n'
+output_text = output_text+F'Greatest Increase in Profits: {greatests_increase_month} (${greatests_increase})\n'
+output_text = output_text+F'Greatest Decrease in Profits: {greatests_decrease_month} (${greatests_decrease})\n'
+
 # Print output to terminal
-print(F'Financial Analysis')
-print(F'----------------------------------------------------')
-print(F'Total Months: {total_months}')
-print(F'Total Profit/Losses: ${total_profit_losses}')
-print(F'Average Change in Profits: ${avg_change}')
-print(F'Greatest Increase in Profits: {greatests_increase_month} (${greatests_increase})')
-print(F'Greatest Decrease in Profits: {greatests_decrease_month} (${greatests_decrease})')
+print(output_text)
 
-# Write output to text file
-
+# Write output to text file & close csv file
 output_path = os.path.join('Analysis','budget_analysis.txt')
 output_file = open(output_path,'wt')
-output_file.write('Financial Analysis'+'\n')
-output_file.write('----------------------------------------------------'+'\n')
-output_file.write(F'Total Months: {total_months}'+'\n')
-output_file.write(F'Total Profit/Losses: ${total_profit_losses}'+'\n')
-output_file.write(F'Average Change in Profits: ${avg_change}'+'\n')
-output_file.write(F'Greatest Increase in Profits: {greatests_increase_month} (${greatests_increase})'+'\n')
-output_file.write(F'Greatest Decrease in Profits: {greatests_decrease_month} (${greatests_decrease})'+'\n')
+output_file.write(output_text)
+output_file.close()
--- Python_Challenge/PyPoll/main.py
+++ Python_Challenge/PyPoll/main.py
@@ -11,40 +11,53 @@ with open(election_path,'rt') as election_file:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(election_file, delimiter=',')
 
-    # Read headers into a variable for reference while coding
+    # Read headers into a variable
     election_header = next(csvreader)
-    print(f"Header: {election_header}")
+    #print(f"Header: {election_header}")
 
     # Create dictionary
-    election_dict = {}
+    results_dict = {}
 
     for row in csvreader:
-        # Read csv into dctionary
-        election_dict[row[0]] = {election_header[1]: row[1], election_header[2]: row[2]}
+        # If candidate is in dictionary, incriment vote count by 1.
+        if row[2] in results_dict.keys():
+           results_dict[row[2]]["Votes"] += 1
+        # If the candidate is not in dictionary, add it and set votes to 1
+        else:
+            results_dict[row[2]] = {"Votes":1}
+    
+    #close csv file
+    election_file.close()
 
-    total_votes = 0
-    winner_name = 'TBD'
+# The total number of votes cast
+total_votes = sum([results_dict[row]["Votes"] for row in results_dict])
 
-    # The total number of votes cast
-    # A complete list of candidates who received votes
-    # The percentage of votes each candidate won
-    # The total number of votes each candidate won
-    # The winner of the election based on popular vote
+
+# A complete list of candidates who received votes, their percentage of votes, & their total votes
+candidates = ''
+for row in results_dict:
+    percent_votes = "{:3.3f}".format(results_dict[row]["Votes"] /total_votes * 100)
+    candidates += f'{row}: {percent_votes}% ({results_dict[row]["Votes"]})\n'
+
+# The winner of the election based on popular vote
+max_votes = max([results_dict[row]["Votes"] for row in results_dict])
+winner_name = [row for row in results_dict if results_dict[row]["Votes"] == max_votes][0]
 
 # Build output text
-output_text = 'Election Results'+'\n'
-output_text = output_text+F'----------------------------------------------------'+'\n'
-output_text = output_text+F'Total Votes: {total_votes}'+'\n'
-output_text = output_text+F'----------------------------------------------------'+'\n'
-# Insert list of candidates & vote percentage
-output_text = output_text+F'----------------------------------------------------'+'\n'
-output_text = output_text+F'Winner: {winner_name}'+'\n'
-output_text = output_text+F'----------------------------------------------------'+'\n'
+output_text = 'Election Results\n'
+output_text = output_text+F'----------------------------------------------------\n'
+output_text = output_text+F'Total Votes: {total_votes}\n'
+output_text = output_text+F'----------------------------------------------------\n'
+output_text = output_text+candidates
+output_text = output_text+F'----------------------------------------------------\n'
+output_text = output_text+F'Winner: {winner_name}\n'
+output_text = output_text+F'----------------------------------------------------'
 
 # Print output to terminal
 print(output_text)
 
-# Write output to text file
+# Write output to text file & close csv file
 output_path = os.path.join('Analysis','election_analysis.txt')
 output_file = open(output_path,'wt')
 output_file.write(output_text)
+output_file.close()
